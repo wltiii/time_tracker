3e55a87ffdc5affb17b46d8cb5697b9a62181866
wltiii <myX2bear>
2022-12-21 14:54:26 -0600

    working...

--- lib/domain/time_entries/time_entry.dart
+++ lib/domain/time_entries/time_entry.dart
@@ -1,7 +1,11 @@
+import 'package:time_tracker/domain/time_entries/time_entry_model.dart';
+
 class TimeEntry {
   TimeEntry({
     required this.id,
+    required this.model,
   });
 
   final int id;
+  final TimeEntryModel model;
 }
--- test/domain/time_entries/time_entry_test.dart
+++ test/domain/time_entries/time_entry_test.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker/domain/time_entries/time_entry.dart';
+import 'package:time_tracker/domain/time_entries/time_entry_model.dart';
 
 void main() {
   group('construction', () {
     test('from default', () {
-      var startTime = DateTime.now();
-      expect(TimeEntry(id: 1), isA<TimeEntry>());
+      var startTime = DateTime.now().subtract(const Duration(hours: 1));
+      var endTime = DateTime.now();
+      var model = TimeEntryModel(start: startTime, end: endTime);
+
+      var entity = TimeEntry(id: 1, model: model);
+
+      expect(entity, isA<TimeEntry>());
     });
   });
 }
