854cce7db7899e488fadb0f22452aeda95fa36c3
wltiii <myX2bear>
2023-01-08 15:46:37 -0600

    WIP: updated test

--- test/domain/time_entries/time_entry_test.dart
+++ test/domain/time_entries/time_entry_test.dart
@@ -3,17 +3,26 @@ import 'package:time_tracker/domain/time_entries/time_entry.dart';
 import 'package:time_tracker/domain/time_entries/time_entry_model.dart';
 import 'package:time_tracker/domain/time_entries/value_objects/end_time.dart';
 import 'package:time_tracker/domain/time_entries/value_objects/start_time.dart';
+import 'package:time_tracker/domain/time_entries/value_objects/time_entry_id.dart';
 
 void main() {
   group('construction', () {
     test('from default', () {
-      final startTime = StartTime(dateTime: DateTime.now().subtract(const Duration(hours: 1)));
-      final endTime = EndTime(dateTime: DateTime.now());
+      final startDateTime = DateTime.now().subtract(const Duration(hours: 1));
+      final startTime = StartTime(dateTime: startDateTime);
+      final endDateTime = DateTime.now();
+      final endTime = EndTime(dateTime: endDateTime);
       final givenModel = TimeEntryModel(start: startTime, end: endTime);
 
-      final result = TimeEntry(id: 1, model: givenModel);
+      // final result = TimeEntry(id: TimeEntryId('abc123'), model: givenModel);
+      final result = TimeEntry(
+        id: TimeEntryId('abc123'),
+        start: startDateTime,
+        end: endDateTime,
+      );
 
       expect(result, isA<TimeEntry>());
+      expect(result.id, equals('abc123'));
       final resultModel = TimeEntryModel(
         start: result.start,
         end: result.end,
@@ -26,15 +35,34 @@ void main() {
   group('json', () {
     test('from/to', () {
       fail('Not yet implemented.');
-      // final givenTimeEntry = TimeEntryModel(
-      //   start: DateTime.now().subtract(
-      //     const Duration(hours: 1),
-      //   ),
-      //   end: DateTime.now(),
-      // );
-      //
+
+      final givenTimeEntry = TimeEntryModel(
+        start: StartTime(
+            dateTime: DateTime.now().subtract(
+          const Duration(hours: 1),
+        )),
+        end: EndTime(dateTime: DateTime.now()),
+      );
+
+      /// TODO(wltiii): should this raise all model objects to top level, or should entity json have a model object?
+      /// i.e
+      ///
+      /// {
+      ///     id: 23456789
+      ///     start: 2023-01-05T13:55:54.163113,
+      ///     end: 2023-01-05T14:55:54.164525
+      /// }
+      /// - or -
+      /// {
+      ///     id: 23456789
+      ///     model: {
+      ///         start: 2023-01-05T13:55:54.163113,
+      ///         end: 2023-01-05T14:55:54.164525
+      ///     }
+      /// }
+
       // expect(
-      //   TimeEntryModel.fromJson(givenTimeEntry.toJson()),
+      //   TimeEntry.fromJson(givenTimeEntry.toJson()),
       //   equals(givenTimeEntry),
       // );
     });
