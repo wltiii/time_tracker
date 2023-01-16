fcd3d1bce59c6c1e34842e3ddb2c6314828783af
wltiii <myX2bear>
2023-01-16 09:46:14 -0600

    time_entry_service.dart: added method dateRangeConsistencyValidator to verify entry does not overlap any existing entries.

--- lib/application/services/time_entry_service.dart
+++ lib/application/services/time_entry_service.dart
@@ -1,23 +1,52 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:time_tracker/application/repositories/time_entry_repository.dart';
+import 'package:time_tracker/domain/error/additional_info.dart';
 import 'package:time_tracker/domain/error/failures.dart';
 import 'package:time_tracker/domain/time_entries/time_entry.dart';
 import 'package:time_tracker/domain/time_entries/time_entry_model.dart';
 
 //TODO(wltiii): WIP
 class TimeEntryService {
-  final TimeEntryRepository _repository;
   TimeEntryService(this._repository);
-  Future<Either<Failure, TimeEntry>> createTimeEntry(TimeEntryModel model) async {
-    final existingTimeEntries = await _repository.getAllForTask(model.taskId);
-    // final newTimeEntry = TimeEntry(model);
-    final newTimeEntry = TimeEntry(
-      start: model.start.dateTime,
-      end: model.end.dateTime,
+
+  final TimeEntryRepository _repository;
+
+  // TODO(wltiii): getAllFor(what exactly?). User, I would think.
+  // TODO(wltiii): for the MVP, the invariant should be User,
+  // TODO(wltiii): other usecases might consider overlapping entries,
+  // TODO(wltiii): enforcing only those that cannot overlap, by the other things tracked (Project, Client, Task, other?)
+  Future<Either<Failure, bool>> dateRangeConsistencyValidator(
+      TimeEntryModel model) async {
+    final result = await _repository.getTimeboxedEntries(
+      start: model.start,
+      end: model.end,
     );
-    for (final timeEntry in existingTimeEntries) {
-      newTimeEntry.overlapsWith(timeEntry);
+
+    Failure? failure;
+    var entries = <TimeEntry>[];
+
+    result.fold(
+      (l) => failure = l,
+      (r) => entries = r,
+    );
+
+    if (result.isLeft()) {
+      return Either.left(failure!);
     }
-    return await _repository.add(newTimeEntry);
+
+    for (final timeEntry in entries) {
+      if (model.overlapsWith(timeEntry.timeEntryRange)) {
+        return Future.value(
+          Left(
+            InconsistencyFailure(
+                AdditionalInfo('Time entry overlaps with existing entry having '
+                    'start ${timeEntry.start.iso8601String} and '
+                    'end ${timeEntry.end.iso8601String}.')),
+          ),
+        );
+      }
+    }
+
+    return Either.right(true);
   }
 }
