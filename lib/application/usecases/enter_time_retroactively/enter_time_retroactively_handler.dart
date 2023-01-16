49ee0f7c76b5e2ecd559e23e1bf42dfc39770e93
wltiii <myX2bear>
2023-01-16 09:49:05 -0600

    enter_time_retroactively_action.dart: WIP enter_time_retroactively_command.dart: WIP enter_time_retroactively_handler.dart: WIP

--- lib/application/usecases/enter_time_retroactively/enter_time_retroactively_handler.dart
+++ lib/application/usecases/enter_time_retroactively/enter_time_retroactively_handler.dart
@@ -1,28 +1,33 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:time_tracker/application/repositories/time_entry_repository.dart';
-import 'package:time_tracker/application/usecases/enter_time_retroactively/enter_time_retroactively_command.dart';
-import 'package:time_tracker/application/usecases/use_case_handler.dart';
 import 'package:time_tracker/domain/error/failures.dart';
 import 'package:time_tracker/domain/time_entries/time_entry.dart';
+import 'package:time_tracker/domain/time_entries/time_entry_model.dart';
 
 class EnterTimeRetroactivelyHandler
-    implements UseCaseHandler<EnterTimeRetroactivelyCommand, TimeEntry> {
+// implements UseCaseHandler<EnterTimeRetroactivelyCommand, TimeEntry> {
+/*implements
+        UseCaseHandler<TimeEntryModel, TimeEntry>*/
+{
   EnterTimeRetroactivelyHandler(this._repository);
 
   final TimeEntryRepository _repository;
 
   @override
-  Future<Either<Failure, TimeEntry>> handle(EnterTimeRetroactivelyCommand command) async {
+  // Future<Either<Failure, TimeEntry>> handle(EnterTimeRetroactivelyCommand command) async {
+  Future<Either<Failure, TimeEntry>> handle(
+      TimeEntryModel timeEntryModel) async {
     //TODO(wltiii): two options for creating TimeEntry (as I have developed at present). All parameters to create a model, or both? Not sure I like either option
     //TODO(wltiii): another option would be to eliminate the Command object altogether and take the model. What are the consequences of this?
-    final timeEntry = TimeEntry(
-      id: command.id,
-      start: command.timeEntryModel.start.dateTime,
-      end: command.timeEntryModel.end.dateTime,
-    );
+    // final timeEntry = TimeEntry(
+    //   id: command.id,
+    //   start: command.timeEntryModel.start.dateTime,
+    //   end: command.timeEntryModel.end.dateTime,
+    // );
     // final timeEntry = TimeEntry(
     //   id: null,
     //   model: command.timeEntryModel,
+    //   // command.timeEntryModel.userId,
     //   // command.timeEntryModel.projectId,
     //   // command.timeEntryModel.taskId,
     //   // command.timeEntryModel.startTime,
@@ -30,6 +35,15 @@ class EnterTimeRetroactivelyHandler
     //   // command.timeEntryModel.description,
     // );
 
-    return await _repository.update(timeEntry);
+    // TODO(wltiii): get the time entry
+    // TODO(wltiii): call update on the entity - no repository
+    // TODO(wltiii): calls should be made from the entity layer
+    return await _repository.update(
+      TimeEntry(
+        id: '1',
+        start: timeEntryModel.start.dateTime,
+        end: timeEntryModel.end.dateTime,
+      ),
+    );
   }
 }
