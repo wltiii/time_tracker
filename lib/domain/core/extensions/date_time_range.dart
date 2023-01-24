ccfac3d0ec42017ae6d039686a336466e487c8fa
wltiii <myX2bear>
2023-01-24 16:47:57 -0600

    date_time_range.dart: fixed bug in isOverlapping()

--- lib/domain/core/extensions/date_time_range.dart
+++ lib/domain/core/extensions/date_time_range.dart
@@ -5,7 +5,16 @@ extension OverlappingRange on DateTimeRange {
   /// Validates that this DateTimeRange instance is within the range, inclusive.
   /// Returns [bool] true when it is within the range inclusive, false otherwise.
   bool isOverlapping(DateTimeRange other) {
-    if (start.inRange(other) || end.inRange(other)) {
+    if (start.inRange(other)) {
+      return true;
+    }
+    if (end.inRange(other)) {
+      return true;
+    }
+    if (other.start.inRange(this)) {
+      return true;
+    }
+    if (other.end.inRange(this)) {
       return true;
     }
     return false;
--- test/domain/core/extensions/date_time_range_test.dart
+++ test/domain/core/extensions/date_time_range_test.dart
@@ -4,64 +4,63 @@ import 'package:time_tracker/domain/core/extensions/date_time_range.dart';
 
 void main() {
   group('DateTimeRange extension tests', () {
+    final now = DateTime.now();
+    final nowPlusOneHour = now.add(const Duration(hours: 1));
+    final nowPlusTwoHours = now.add(const Duration(hours: 2));
+    final nowPlusThreeHours = now.add(const Duration(hours: 3));
+
     group('overlapsWith tests', () {
       test('this range before other returns false', () {
-        final thisStart = DateTime.now();
-        final thisEnd = thisStart.add(const Duration(days: 1));
-        final otherStart = thisStart.add(const Duration(days: 2));
-        final otherEnd = thisStart.add(const Duration(days: 3));
-
-        final thisRange = DateTimeRange(start: thisStart, end: thisEnd);
-        final otherRange = DateTimeRange(start: otherStart, end: otherEnd);
+        final thisRange = DateTimeRange(start: now, end: nowPlusOneHour);
+        final otherRange =
+            DateTimeRange(start: nowPlusTwoHours, end: nowPlusThreeHours);
 
         expect(thisRange.isOverlapping(otherRange), isFalse);
       });
 
       test('this range after other returns false', () {
-        final thisStart = DateTime.now();
-        final thisEnd = thisStart.add(const Duration(days: 1));
-        final otherStart = thisStart.subtract(const Duration(days: 2));
-        final otherEnd = thisStart.subtract(const Duration(days: 1));
-
-        final thisRange = DateTimeRange(start: thisStart, end: thisEnd);
-        final otherRange = DateTimeRange(start: otherStart, end: otherEnd);
+        final thisRange =
+            DateTimeRange(start: nowPlusTwoHours, end: nowPlusThreeHours);
+        final otherRange = DateTimeRange(start: now, end: nowPlusOneHour);
 
         expect(thisRange.isOverlapping(otherRange), isFalse);
       });
 
       test('ranges with same start/end returns true', () {
-        final thisStart = DateTime.now();
-        final thisEnd = thisStart.add(const Duration(days: 1));
-        final otherStart = DateTime.fromMicrosecondsSinceEpoch(
-            thisStart.microsecondsSinceEpoch);
-        final otherEnd = thisStart.add(const Duration(days: 1));
-
-        final thisRange = DateTimeRange(start: thisStart, end: thisEnd);
-        final otherRange = DateTimeRange(start: otherStart, end: otherEnd);
+        final thisRange = DateTimeRange(start: now, end: nowPlusOneHour);
+        final otherRange = DateTimeRange(start: now, end: nowPlusOneHour);
 
         expect(thisRange.isOverlapping(otherRange), isTrue);
       });
 
-      test('start overlaps other range returns true', () {
-        final thisStart = DateTime.now();
-        final thisEnd = thisStart.add(const Duration(days: 1));
-        final otherStart = thisStart.subtract(const Duration(hours: 1));
-        final otherEnd = thisEnd.subtract(const Duration(hours: 1));
-
-        final thisRange = DateTimeRange(start: thisStart, end: thisEnd);
-        final otherRange = DateTimeRange(start: otherStart, end: otherEnd);
+      test('this range contains other range returns true', () {
+        final thisRange = DateTimeRange(start: now, end: nowPlusThreeHours);
+        final otherRange =
+            DateTimeRange(start: nowPlusOneHour, end: nowPlusTwoHours);
 
         expect(thisRange.isOverlapping(otherRange), isTrue);
       });
 
-      test('end overlaps other range returns true', () {
-        final thisStart = DateTime.now();
-        final thisEnd = thisStart.add(const Duration(days: 1));
-        final otherStart = thisStart.add(const Duration(hours: 1));
-        final otherEnd = thisEnd.add(const Duration(hours: 1));
+      test('range contained within other range returns true', () {
+        final thisRange =
+            DateTimeRange(start: nowPlusOneHour, end: nowPlusTwoHours);
+        final otherRange = DateTimeRange(start: now, end: nowPlusThreeHours);
 
-        final thisRange = DateTimeRange(start: thisStart, end: thisEnd);
-        final otherRange = DateTimeRange(start: otherStart, end: otherEnd);
+        expect(thisRange.isOverlapping(otherRange), isTrue);
+      });
+
+      test('start within other range returns true', () {
+        final thisRange =
+            DateTimeRange(start: nowPlusOneHour, end: nowPlusThreeHours);
+        final otherRange = DateTimeRange(start: now, end: nowPlusTwoHours);
+
+        expect(thisRange.isOverlapping(otherRange), isTrue);
+      });
+
+      test('end within other range returns true', () {
+        final thisRange = DateTimeRange(start: now, end: nowPlusTwoHours);
+        final otherRange =
+            DateTimeRange(start: nowPlusOneHour, end: nowPlusThreeHours);
 
         expect(thisRange.isOverlapping(otherRange), isTrue);
       });
