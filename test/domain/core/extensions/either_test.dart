21cfbb810d7853d141c836ccd7811d1b02950dda
wltiii <myX2bear>
2023-01-26 14:13:33 -0600

    added missing tests

---
+++ test/domain/core/extensions/either_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:time_tracker/domain/core/extensions/either.dart';
+
+void main() {
+  test('returns right', () {
+    const right = 1;
+
+    Either<String, int> e = const Right(right);
+
+    expect(e.right(), equals(right));
+    expect(e.left(), isNull);
+  });
+
+  test('returns left', () {
+    const left = 'left';
+
+    Either<String, int> e = const Left(left);
+
+    expect(e.right(), isNull);
+    expect(e.left(), equals(left));
+  });
+}
