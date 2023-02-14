7a24d42da0fd31840eed0cbdd45ffc6a67e74bd4
wltiii <myX2bear>
2023-02-14 13:11:31 -0600

    added test verifying that when existing timer is not running a failure is thrown

--- test/application/usecases/stop_timer/stop_timer_action_test.dart
+++ test/application/usecases/stop_timer/stop_timer_action_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker/application/usecases/stop_timer/stop_timer_action.dart';
 import 'package:time_tracker/domain/core/extensions/date_time.dart';
 import 'package:time_tracker/domain/core/extensions/either.dart';
+import 'package:time_tracker/domain/error/additional_info.dart';
+import 'package:time_tracker/domain/error/failures.dart';
 import 'package:time_tracker/domain/time_entries/time_entry_model.dart';
 import 'package:time_tracker/domain/time_entries/value_objects/end_time.dart';
 import 'package:time_tracker/domain/time_entries/value_objects/start_time.dart';
@@ -75,20 +77,48 @@ void main() {
     });
 
     test('when existing timer is not running throw', () async {
-      fail('Not yet implemented');
-      // final repository = TimeEntryRepositoryMock();
-      // final action = StopTimerAction(repository);
-      //
-      // final result = await action();
-      //
-      // result.fold(
-      //   (l) {
-      //     fail('Creating a timer should not fail.');
-      //   },
-      //   (r) {
-      //     expect(r.id, equals(TimeEntryId('1')));
-      //   },
-      // );
+      final expectedFailure = InvalidStateFailure(
+        AdditionalInfo('Cannot stop a timer that is already stopped.'),
+      );
+      final repository = TimeEntryRepositoryMock();
+      final givenExistingStartTime =
+          StartTime(dateTime: DateTime.now().subtract(const Duration(days: 7)));
+      final givenExistingEndTime = EndTime(dateTime: DateTime.now());
+      final givenExistingEntryModel = TimeEntryModel(
+        startTime: givenExistingStartTime,
+        endTime: givenExistingEndTime,
+      );
+
+      final givenExistingTimeEntry =
+          await repository.add(givenExistingEntryModel);
+
+      if (givenExistingTimeEntry.isLeft()) {
+        fail(
+          'Test setup failure. Could not add time entry to '
+          'repository. TimeEntry: $givenExistingEntryModel',
+        );
+      }
+
+      print('initial entry created:');
+      repository.showPersistedEntries();
+
+      final action = StopTimerAction(repository);
+      final result = await action(givenExistingTimeEntry.right()!.id);
+
+      print('stop time action completed:');
+      repository.showPersistedEntries();
+
+      result.fold(
+        (l) {
+          expect(l, isA<Failure>());
+          expect(l, isA<InvalidStateFailure>());
+          InvalidStateFailure f = l as InvalidStateFailure;
+          expect('$f', equals('$expectedFailure'));
+        },
+        (r) {
+          fail('Updating a timer should not fail.');
+        },
+      );
     });
 
     test('when stop time is less than start it throws', () async {
