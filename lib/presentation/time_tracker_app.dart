a8d797772d8b71bb462e71aca611840ea2979f34
wltiii <myX2bear>
2023-02-18 19:03:46 -0600

    WIP: initial pass at migrating to single start/stop button

--- lib/presentation/time_tracker_app.dart
+++ lib/presentation/time_tracker_app.dart
@@ -20,7 +20,7 @@ class TimeTrackerApp extends StatelessWidget {
     // TODO(wltiii): Does this have to be nullable? Possibly the Either?
     final ValueNotifier<DateTime?> startTime = ValueNotifier(null);
     final ValueNotifier<DateTime?> stopTime = ValueNotifier(null);
-    TimeEntryId? timeEntryId;
+    TimeEntryId? runningTimerId;
 
     return MaterialApp(
       home: Scaffold(
@@ -40,6 +40,22 @@ class TimeTrackerApp extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                // icon: Icons.play,
+                // icon: Icons.stop,
+                // icon: Icons.restart_alt,
+                // if (timeEntryId == null) {
+                //   showStartButton();
+                // }
+                // else {
+                //   showStopButton();
+                // }
+
+                // startButton(
+                //   ValueNotifier<DateTime?> startTime,
+                //   TextEditingController startTimeController,
+                //   TimeEntryId? runningTimerId,
+                // );
+
                 ElevatedButton(
                   child: const Text('Start'),
                   onPressed: () async {
@@ -50,25 +66,26 @@ class TimeTrackerApp extends StatelessWidget {
                       (startedTimer) {
                         //TODO(wltiii): use StartTime rather than dateTime???
                         startTime.value = startedTimer.start.dateTime;
-                        timeEntryId = startedTimer.id;
+                        runningTimerId = startedTimer.id;
                         startTimeController.text =
                             _formatDateTime(startedTimer.start.dateTime);
                       },
                     );
                   },
                 ),
+
                 const SizedBox(width: 20),
                 ElevatedButton(
                   child: const Text('Stop'),
                   onPressed: () async {
                     // TODO(wltiii): There should be one button and it switches state as appropriate obviating the need for this check
-                    if (timeEntryId != null) {
-                      final result = await _stopTimerAction(timeEntryId!);
+                    if (runningTimerId != null) {
+                      final result = await _stopTimerAction(runningTimerId!);
                       result.fold(
                         //TODO(wltiii): handle failure
                         (failure) => {},
                         (stoppedTimer) {
-                          timeEntryId = null;
+                          runningTimerId = null;
                           //TODO(wltiii): use EndTime rather than dateTime???
                           stopTime.value = stoppedTimer.end.dateTime;
                           stopTimeController.text =
@@ -137,6 +154,31 @@ class TimeTrackerApp extends StatelessWidget {
       ),
     );
   }
+
+  // Widget startButton(
+  //   TimeEntry startedTimer,
+  //   ValueNotifier<DateTime?> startTime,
+  //   TextEditingController startTimeController,
+  //   TimeEntryId? runningTimerId,
+  // ) {
+  //   return ElevatedButton(
+  //     child: const Text('Start'),
+  //     onPressed: () async {
+  //       final result = await _startTimerAction();
+  //       result.fold(
+  //         //TODO(wltiii): handle failure
+  //         (failure) => {},
+  //         (startedTimer) {
+  //           //TODO(wltiii): use StartTime rather than dateTime???
+  //           startTime.value = startedTimer.start.dateTime;
+  //           runningTimerId = startedTimer.id;
+  //           startTimeController.text =
+  //               _formatDateTime(startedTimer.start.dateTime);
+  //         },
+  //       );
+  //     },
+  //   );
+  // }
 
   Widget resetButton(
       ValueNotifier<DateTime?> startTime,
