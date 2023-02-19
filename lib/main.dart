16504e94b1e3b161399198b66ae2dd573cc07365
wltiii <myX2bear>
2023-02-19 08:28:27 -0600

    added Firebase app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/presentation/time_tracker_app.dart';
 
-void main() => runApp(TimeTrackerApp());
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(TimeTrackerApp());
+}
