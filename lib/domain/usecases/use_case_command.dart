0a459c593a9049ac22862083177244e0b42e0a02
wltiii <myX2bear>
2023-04-18 14:02:17 -0500

    updated comments re: command pattern

--- lib/domain/usecases/use_case_command.dart
+++ lib/domain/usecases/use_case_command.dart
@@ -1,3 +1,6 @@
-//TODO(wltiii): does this provide any value?
 //TODO(wltiii): so far I am not using commands and handlers - consider deleting
+//TODO(wltiii): these are parts of the Command pattern. It seems overkill for this app.
+//TODO(wltiii): see the following posts for more info:
+//TODO(wltiii): 1.  https://medium.com/@mgonzalezbaile/implementing-a-use-case-i-intro-38c80b4fed0
+//TODO(wltiii): 2.  https://medium.com/@mgonzalezbaile/implementing-a-use-case-ii-command-pattern-2d49d980e61c
 abstract class UseCaseCommand<T> {}
--- lib/domain/usecases/use_case_handler.dart
+++ lib/domain/usecases/use_case_handler.dart
@@ -2,8 +2,11 @@ import 'package:fpdart/fpdart.dart';
 import 'package:time_tracker/domain/core/error/failures.dart';
 import 'package:time_tracker/domain/usecases/use_case_command.dart';
 
-//TODO(wltiii): does this provide any value?
 //TODO(wltiii): so far I am not using commands and handlers - consider deleting
+//TODO(wltiii): these are parts of the Command pattern. It seems overkill for this app.
+//TODO(wltiii): see the following posts for more info:
+//TODO(wltiii): 1.  https://medium.com/@mgonzalezbaile/implementing-a-use-case-i-intro-38c80b4fed0
+//TODO(wltiii): 2.  https://medium.com/@mgonzalezbaile/implementing-a-use-case-ii-command-pattern-2d49d980e61c
 abstract class UseCaseHandler<C extends UseCaseCommand<T>, T> {
   Future<Either<Failure, T>> handle(C command);
 }
